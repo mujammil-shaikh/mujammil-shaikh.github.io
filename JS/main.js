5350bded506c4c2bfd84ddaeca269041045cd402
mujammil-shaikh <61930769+mujammil-shaikh@users.noreply.github.com>
2023-07-03 01:05:27 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+// Add this JavaScript code to handle the click event on the menu icon
+
+document.querySelector(".menu-icon").addEventListener("click", function() {
+    document.querySelector("nav").classList.toggle("active");
+  });
+  
